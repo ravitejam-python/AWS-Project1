e1c2ad48c5eadd6365e70425c38e7c5bc90b2588
ravitejam-python <72593808+ravitejam-python@users.noreply.github.com>
2021-03-02 16:46:02 +0530

    Config file added

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "raviteja.c3owxrrndqtq.us-east-1.rds.amazonaws.com"
+customuser = "Raviteja"
+custompass = "Raviteja"
+customdb = "employee"
+custombucket = "addemployee-ravi"
+customregion = "us-east-1"
